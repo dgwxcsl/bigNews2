8efd67f6b6e5b4b8a6e0bd9b4494f55e4849f494
gobalDgwxcsl <2226292361@qq.com>
2020-05-16 17:56:25 +0800

    02-登录功能

--- admin/js/login.js
+++ admin/js/login.js
@@ -1,5 +1,40 @@
 $(function () {
-    $.ajax({
-        type:''
+    var login_form = $('.login_form')
+    login_form.on('submit', function (e) {
+        e.preventDefault()
+        console.log($(this).serialize());
+        $.ajax({
+            type: 'post',
+            url: BigNew.user_login,
+            data: $(this).serialize(),
+            beforeSend: function () {
+                var flag = false
+                $('.login_form input[name]').each(function (index, ele) {
+                    if ($.trim($(ele).val()) == '') {
+                        flag = true
+                    }
+                })
+                if (flag) {
+                    $('.modal-body p').text('输入的密码不能为空')
+                    $('#myModal').modal('show')                  
+                    return false
+                }
+            },
+            success: function (res) {
+                console.log(res);
+                $('.modal-body p').text(res.msg)
+                $('#myModal').modal('show')
+
+                if (res.code === 200) {
+                    $('.modal').on('hidden.bs.modal', function () {
+                        window.localStorage.setItem('token', res.token)
+                        window.location.href = './index.html'
+                    })
+                }
+
+
+
+            }
+        })
     })
 })
